03f5dea2dd55e3be0ce8abaaa3a3dc79ef8fbbf4
bktid <tim.demeyer@cegeka.be>
2015-09-18 09:54:03 +0200

    Jo, Tim: aggregateIdGenerator: eerste aanzet + gewired op BestellingFactory

--- src/main/java/be/swsb/fiazard/bestelling/BestellingFactory.java
+++ src/main/java/be/swsb/fiazard/bestelling/BestellingFactory.java
@@ -1,9 +1,20 @@
 package be.swsb.fiazard.bestelling;
 
+import com.google.common.base.Preconditions;
+
+import be.swsb.fiazard.event.AggregateIdGenerator;
+
 class BestellingFactory {
 	
+	private AggregateIdGenerator aggregateIdGenerator;
+	
+	public BestellingFactory(AggregateIdGenerator aggregateIdGenerator) {
+		Preconditions.checkArgument(aggregateIdGenerator != null);
+		this.aggregateIdGenerator = aggregateIdGenerator;
+	}
+	
 	Bestelling maakNieuwBestellingAan(PlaatsBestellingCommand plaatsBestellingCommand){
-		return new Bestelling (plaatsBestellingCommand.getNaamBesteller());
+		return new Bestelling(aggregateIdGenerator.generate(), plaatsBestellingCommand.getNaamBesteller());
 	}
 	
 	
---
+++ src/main/java/be/swsb/fiazard/event/AggregateIdGenerator.java
@@ -0,0 +1,9 @@
+package be.swsb.fiazard.event;
+
+public class AggregateIdGenerator {
+	
+	public AggregateId generate() {
+		// TODO dees is voor butske en joones om te implementeren;
+		throw new UnsupportedOperationException();
+	}
+}
